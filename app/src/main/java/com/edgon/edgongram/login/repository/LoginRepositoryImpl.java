5b09860e1cffb2db5b2d447c631f2ccd75f447b9
Edwin <edgon85@gmail.com>
2017-08-08 12:49:55 -0500

    commit cambio por errorers, para regresear a uno anterior

---
+++ app/src/main/java/com/edgon/edgongram/login/repository/LoginRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.edgon.edgongram.login.repository;
+
+import com.edgon.edgongram.login.presenters.LoginPresenter;
+
+/**
+ * Created by gonza on 6/28/2017.
+ */
+
+public class LoginRepositoryImpl implements LoginRespository{
+    LoginPresenter presenter;
+
+    public LoginRepositoryImpl(LoginPresenter presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void signIn(String username, String Password) {
+        boolean succes = true;
+        if (succes){
+            presenter.loginSucces();
+        }else{
+            presenter.loginError("Ocurrió un Error");
+        }
+    }
+}
---
+++ app/src/main/java/com/edgon/edgongram/post/view/NewPostActivity.java
@@ -0,0 +1,15 @@
+package com.edgon.edgongram.post.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edgon.edgongram.R;
+
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
