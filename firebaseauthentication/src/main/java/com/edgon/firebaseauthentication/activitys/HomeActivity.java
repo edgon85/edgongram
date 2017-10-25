273b40e9c897c98e328e4bdbdc8c3dbee8fe0cb6
Edwin Gonzalez <edgon85@gmail.com>
2017-10-25 18:22:11 -0500

    autenticacion con google, configurado y funcional

--- firebaseauthentication/src/main/java/com/edgon/firebaseauthentication/activitys/HomeActivity.java
+++ firebaseauthentication/src/main/java/com/edgon/firebaseauthentication/activitys/HomeActivity.java
@@ -10,14 +10,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.edgon.firebaseauthentication.R;
 import com.edgon.firebaseauthentication.login.login.view.LoginActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
     private static final String TAG = HomeActivity.class.getSimpleName();
     FloatingActionButton fab;
@@ -26,6 +35,9 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView txtUserId;
     private TextView txtUserEmail;
+    private ImageView imgPhoto;
+
+    GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,7 @@ public class HomeActivity extends AppCompatActivity {
         fab = (FloatingActionButton) findViewById(R.id.fab);
         txtUserId = (TextView) findViewById(R.id.txt_usuario_id);
         txtUserEmail = (TextView) findViewById(R.id.txt_usuario_email);
+        imgPhoto = (ImageView) findViewById(R.id.img_photo);
 
         buttonFloating();
     }
@@ -51,6 +64,20 @@ public class HomeActivity extends AppCompatActivity {
 
     private void logOunt() {
         firebaseAuth.signOut();
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()){
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(HomeActivity.this, "Error in Goole sign out", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
@@ -79,11 +106,27 @@ public class HomeActivity extends AppCompatActivity {
                 if (firebaseUser != null) {
                     txtUserId.setText(firebaseUser.getUid());
                     txtUserEmail.setText(firebaseUser.getEmail());
+
+                    Picasso.with(HomeActivity.this)
+                            .load(firebaseUser.getPhotoUrl())
+                            .into(imgPhoto);
+
                 } else {
                     Log.w(TAG, "onAuthStateChanged - signed_out");
                 }
             }
         };
+
+        //Initialization google account
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
     @Override
@@ -96,5 +139,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- firebaseauthentication/src/main/java/com/edgon/firebaseauthentication/login/login/view/LoginActivity.java
+++ firebaseauthentication/src/main/java/com/edgon/firebaseauthentication/login/login/view/LoginActivity.java
@@ -8,27 +8,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.edgon.firebaseauthentication.R;
 import com.edgon.firebaseauthentication.activitys.HomeActivity;
 import com.edgon.firebaseauthentication.login.create_account.view.CreateAccountActivity;
 import com.edgon.firebaseauthentication.login.login.presenter.LoginPresenter;
 import com.edgon.firebaseauthentication.login.login.presenter.LoginPresenterImpl;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity implements LoginView{
+public class LoginActivity extends AppCompatActivity
+        implements LoginView, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     private static final String TAG = "LoginActivity";
+    private static final int SIGN_IN_GOOGLE_CODE = 1;
     FirebaseAuth firebaseAuth;
     FirebaseAuth.AuthStateListener authStateListener;
 
+    GoogleApiClient googleApiClient;
+
     private TextInputEditText edtUser;
     private TextInputEditText edtPassword;
-    private Button btnLogin;
     private TextView btnCreateAcount;
+    private Button btnLogin;
+    private SignInButton btnLoginGoogle;
 
     private LoginPresenter presenter;
 
@@ -41,32 +57,18 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
         edtPassword = (TextInputEditText) findViewById(R.id.password_login);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnCreateAcount = (TextView) findViewById(R.id.txt_go_create_account);
+        btnLoginGoogle = (SignInButton) findViewById(R.id.btn_login_google);
 
-        inicialize();
+        initialize();
         presenter = new LoginPresenterImpl(this);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String email = edtUser.getText().toString();
-                String password = edtPassword.getText().toString();
-
-                Log.w(TAG, email + " " + password);
-                login(email,password);
-            }
-        });
-
-        btnCreateAcount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goCreateAcount();
-            }
-        });
+        btnLogin.setOnClickListener(this);
+        btnCreateAcount.setOnClickListener(this);
+        btnLoginGoogle.setOnClickListener(this);
 
     }
 
-    private void inicialize() {
+    private void initialize() {
         firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -83,11 +85,68 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
                 }
             }
         };
+
+        //Initialization google account
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
 
     private void login(String email, String password){
         presenter.signIn(email, password, this);
+    }
+
+    private void loginGoogle(GoogleSignInResult googleSignInResult){
+
+         if (googleSignInResult.isSuccess()){
+             AuthCredential authCredential = GoogleAuthProvider
+                     .getCredential(googleSignInResult.getSignInAccount().getIdToken(),null);
+
+             firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                 @Override
+                 public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Google Authentication success", Toast.LENGTH_SHORT).show();
+                        goHome();
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Google Authentication unsuccess", Toast.LENGTH_SHORT).show();
+                    }
+                 }
+             });
+        }else{
+             Toast.makeText(this, "Google Sign In Unsuccess", Toast.LENGTH_SHORT).show();
+         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_create_account:
+                goCreateAcount();
+                break;
+
+            case R.id.btn_login:
+                String email = edtUser.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                Log.w(TAG, email + " " + password);
+                login(email,password);
+                break;
+
+            case R.id.btn_login_google:
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN_GOOGLE_CODE);
+                break;
+
+        }
     }
 
     @Override
@@ -117,5 +176,23 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
     protected void onStop() {
         super.onStop();
         firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_GOOGLE_CODE){
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi
+                    .getSignInResultFromIntent(data);
+
+            loginGoogle(googleSignInResult);
+        }
+
     }
 }
