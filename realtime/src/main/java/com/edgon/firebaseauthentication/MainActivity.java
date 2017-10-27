8c89af5c890fae84ec688aae12dd84c28f6df7bd
Edwin Gonzalez <edgon85@gmail.com>
2017-10-27 15:16:16 -0500

    agregado y funcional database realtime con firebase.

--- realtime/src/main/java/com/edgon/firebaseauthentication/MainActivity.java
+++ realtime/src/main/java/com/edgon/firebaseauthentication/MainActivity.java
@@ -2,12 +2,118 @@ package com.edgon.firebaseauthentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.edgon.firebaseauthentication.model.Artist;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button btnAddArtist;
+    private ListView listView;
+    private ArrayAdapter arrayAdapter;
+    private List<String> artistNames;
+    private List<Artist> artists;
+
+    private static final String ARTIST_NODE = "Artists";
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Cuando no hay internet y se vuelve a conctar
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        btnAddArtist = (Button) findViewById(R.id.btn_add_artist);
+        listView = (ListView) findViewById(R.id.list_view);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference(); //obtiene una referencia de https://edgongram.firebaseio.com/
+
+
+        artistNames = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,artistNames);
+        artists = new ArrayList<>();
+
+        listView.setAdapter(arrayAdapter);
+
+        leerDatos();
+
+        btnAddArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createArtist();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                deleteData(position);
+                Toast.makeText(MainActivity.this," Eliminado", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
+
+    private void leerDatos() {
+        databaseReference.child(ARTIST_NODE)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        artistNames.clear();
+                        artists.clear();
+
+                        if (dataSnapshot.exists()){
+                            for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                                Artist artist = snapshot.getValue(Artist.class);
+                                Log.w(TAG,"Artist name: " + artist.getName());
+                                artistNames.add(artist.getName());
+                                artists.add(artist);
+                            }
+                        }
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public void createArtist(){
+        Artist artist = new Artist(databaseReference.push().getKey(),"Garbage","Rock");
+
+        databaseReference.child(ARTIST_NODE)
+                .child(artist.getId())
+                .setValue(artist);
+
+        Toast.makeText(this, "Artista agregado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void deleteData(int position){
+        String idArtist = artists.get(position).getId();
+        artists.remove(position);
+        artistNames.remove(position);
+        databaseReference.child(ARTIST_NODE).child(idArtist).removeValue();
+    }
+
+
 }
---
+++ realtime/src/main/java/com/edgon/firebaseauthentication/model/Artist.java
@@ -0,0 +1,51 @@
+package com.edgon.firebaseauthentication.model;
+
+
+public class Artist {
+
+    private String id;
+    private String name;
+    private String genre;
+
+    public Artist() {
+    }
+
+    public Artist(String id, String name, String genre) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
